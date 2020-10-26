936fa93002b9a211e0d0acb6491072ab69f8c02c
Matt <matthewsings5@gmail.com>
2020-10-25 20:34:38 -0400

    Changed text

--- pages/index.jsx
+++ pages/index.jsx
@@ -6,7 +6,7 @@ export default function Home() {
   <div className="w-100 py-12">
     <div className="text-center">
       <h3 className="mt-2 text-3xl text-indigo-600 font-semibold">TechHacks</h3>
-      <h6 className="mt-4 text-xl font-semibold lg:max-auto">A premier Hackathon non-profit based in Brooklyn, NY!</h6>
+      <h6 className="mt-4 text-xl font-semibold lg:max-auto">A high school hackathon non-profit based in Brooklyn, NY!</h6>
     </div>
   </div>) 
 
