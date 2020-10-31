34554ba44f97992155501b7e96c0444c2e82a7ef
Hao Teng <haoteng10@gmail.com>
2020-10-31 14:28:04 -0400

    Refactor React Context

--- contexts/RegistrationModalContext.jsx
+++ contexts/RegistrationModalContext.jsx
@@ -1,3 +1,16 @@
-import { createContext } from "react";
+import React, { createContext, useState, useMemo } from "react";
 
 export const RegistrationModalContext = createContext(null);
+
+const RegistrationModalContextProvider = (props) => {
+    const [registrationModal, setRegistrationModal] = useState(-1);
+    const value = useMemo(() => ({ registrationModal, setRegistrationModal }), [registrationModal, setRegistrationModal]);
+
+    return (
+        <RegistrationModalContext.Provider value={value}>
+            {props.children}
+        </RegistrationModalContext.Provider>
+    )
+}
+
+export default RegistrationModalContextProvider;
--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,17 @@
 import Layout from '../components/Layout';
 import '../styles/globals.css';
 import '../components/Layout';
-import { RegistrationModalContext } from '../contexts/RegistrationModalContext';
-import { useMemo, useState } from 'react';
+import RegistrationModalContextProvider from '../contexts/RegistrationModalContext';
 
 function MyApp({ Component, pageProps }) {
-  const [registrationModal, setRegistrationModal] = useState(-1);
-  const value = useMemo(() => ({ registrationModal, setRegistrationModal }), [registrationModal, setRegistrationModal]);
 
   return (
   <Layout>
-    <RegistrationModalContext.Provider value={value}>
+    <RegistrationModalContextProvider>
       <Component {...pageProps} />
-    </RegistrationModalContext.Provider>
-  </Layout>);
+    </RegistrationModalContextProvider>
+  </Layout>
+  );
 }
 
 export default MyApp
