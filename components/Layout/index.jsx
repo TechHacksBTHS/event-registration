d43caa4fb2892e65a003a045d103fb5397e0fac7
Hao Teng <haoteng10@gmail.com>
2020-10-30 23:28:42 -0400

    Add Title

--- components/Layout/index.jsx
+++ components/Layout/index.jsx
@@ -1,7 +1,12 @@
 import Nav from './Nav';
+import Head from 'next/head';
 
 const Layout = ({ children }) => (
     <div className="overflow-x-hidden">
+      <Head>
+        <title>TechHacks - Register</title>
+      </Head>
+      
       <Nav />
         { children }
     </div>
