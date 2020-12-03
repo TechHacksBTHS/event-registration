5c8c3e760c720cb565855d9f8470f104e6dd4d32
Hao Teng <haoteng10@gmail.com>
2020-12-02 21:31:07 -0500

    Let's try this

--- config/FirebaseAdminConfig.js
+++ config/FirebaseAdminConfig.js
@@ -5,7 +5,7 @@ import firebaseAdmin from 'firebase-admin';
 if (!firebaseAdmin.apps.length) {
   firebaseAdmin.initializeApp({
     credential: firebaseAdmin.credential.cert({
-      privateKey: process.env.FIREBASE_PRIVATE_KEY,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
       projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
     }),
