6019b2390b614c9843608560c14bd606f9c2baee
Shaedil <shaedildider@gmail.com>
2021-02-11 10:38:16 -0500

    feat: add reference links to each sponsor image

--- pages/index.jsx
+++ pages/index.jsx
@@ -9,49 +9,75 @@ export default function Home() {
       <h6 className="mx-48 mt-4 text-xl font-semibold lg:max-auto">As representatives of the nation's largest and most prestigious STEM high school, TechHacks aims to expand student engagement in technology through hackathons, workshops, and computer science events.</h6>
 
       <div className="flex flex-wrap px-24 -mx-4">
-      
+        {/* css animated image here */}
       </div>
 
       <h3 className="mt-16 text-3xl font-bold">Sponsors</h3>
       <div className="flex flex-wrap items-center justify-center px-24 -mx-4 overflow-hidden">
         <div className="min-w-full px-8 my-4 overflow-hidden hover:w-2/3 sm:w-1/2 md:w-1/4 sm:min-w-0">
-          <img src="images/sponsor-logos/msft.png" alt="Microsoft"/>
+          <a href="https://www.microsoft.com/">
+            <img src="images/sponsor-logos/msft.png" alt="Microsoft"/>
+          </a>
         </div>
         <div className="min-w-full px-8 my-4 overflow-hidden hover:w-2/3 sm:w-1/2 md:w-1/4 sm:min-w-0">
+          <a href="https://www.appnexus.com/">
           <img src="images/sponsor-logos/appnexus.png" alt="AppNexus"/>
+          </a>
         </div>
         <div className="min-w-full px-8 my-4 overflow-hidden hover:w-2/3 sm:w-1/2 md:w-1/4 sm:min-w-0">
+          <a href="https://www.vice.com/">
           <img src="images/sponsor-logos/vice-logo-transparent.png" alt="Vice"/>
+          </a>
         </div>
         <div className="min-w-full px-8 my-4 overflow-hidden hover:w-2/3 sm:w-1/2 md:w-1/4 sm:min-w-0">
+          <a href="https://www.bnymellon.com/">
           <img src="images/sponsor-logos/bnymellon.jpg" alt="BNY Mellon"/>
+          </a>
         </div>
         <div className="min-w-full px-8 my-4 overflow-hidden hover:w-2/3 sm:w-1/2 md:w-1/4 sm:min-w-0">
+          <a href="https://www.crimsoneducation.org/">
           <img src="images/sponsor-logos/crimson.jpg" alt="Crimson"/>
+          </a>
         </div>
         <div className="min-w-full px-8 my-4 overflow-hidden hover:w-2/3 sm:w-1/2 md:w-1/4 sm:min-w-0">
+          <a href="https://www.echoar.xyz">
           <img className="w-auto" src="images/sponsor-logos/echoAR-dark.png" alt="echoAR"/>
+          </a>
         </div>
         <div className="min-w-full px-8 my-4 overflow-hidden hover:w-2/3 sm:w-1/2 md:w-1/4 sm:min-w-0">
+          <a href="https://www.sashido.io/">
           <img src="images/sponsor-logos/sashido.png" alt="Sashido"/>
+          </a>
         </div>
         <div className="min-w-full px-8 my-4 overflow-hidden hover:w-2/3 sm:w-1/2 md:w-1/4 sm:min-w-0">
+          <a href="https://www.wolfram.com/">
           <img src="images/sponsor-logos/wolfram.png" alt="Wolfram"/>
+          </a>
         </div>
         <div className="min-w-full px-8 my-4 overflow-hidden hover:w-2/3 sm:w-1/2 md:w-1/4 sm:min-w-0">
+          <a href="https://www.magoosh.com/">
           <img src="images/sponsor-logos/Magoosh.png" alt="Magoosh"/>
+          </a>
         </div>
         <div className="min-w-full px-8 my-4 overflow-hidden hover:w-2/3 sm:w-1/2 md:w-1/4 sm:min-w-0">
+          <a href="https://www.linode.com/">
           <img src="images/sponsor-logos/linode.png" alt="Linode"/>
+          </a>
         </div>
         <div className="min-w-full px-8 my-4 overflow-hidden hover:w-2/3 sm:w-1/2 md:w-1/4 sm:min-w-0">
+          <a href="https://www.balsamiq.com/">
           <img src="images/sponsor-logos/balsamiq.png" alt="Balsamiq"/>
+          </a>
         </div>
         <div className="min-w-full px-8 my-4 overflow-hidden hover:w-2/3 sm:w-1/2 md:w-1/4 sm:min-w-0">
+          <a href="https://www.alpharidge.com/">
           <img src="images/sponsor-logos/alpharidge.png" alt="AlphaRidge"/>
+          </a>
         </div>
         <div className="min-w-full px-8 my-4 overflow-hidden hover:w-2/3 sm:w-1/2 md:w-1/4 sm:min-w-0">
+          <a href="https://www.spotify.com/">
           <img src="images/sponsor-logos/spotify.png" alt="Spotify"/>
+          </a>
         </div>
       </div>
 
